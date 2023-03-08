33e4ee7728828c3ad1f56ee77efe118fcb9a88e3
lizab <belos.liza@gmail.com>
2023-03-08 18:58:19 +0100

    this code cause problems

--- libguile/gc.c
+++ libguile/gc.c
@@ -503,6 +503,7 @@ scm_gc_for_newcell (scm_t_cell_type_statistics *freelist, SCM *free_cells)
 
       //did_gc = 1;
       //scm_i_gc ("cells");
+      // todo : this code cause problems
 
       *free_cells = scm_i_sweep_some_segments (freelist);
     }
